f3007726fc5980ab0150174412bf2eb7ed88cfe3
abjj1999 <abdullahalhilfi21@gmail.com>
2026-07-03 10:20:40 -0500

    Feat: added font and integrated them with nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,33 @@
-import "@/global.css"
+import "@/global.css";
+import { Link } from "expo-router";
+import { styled } from "nativewind";
 import { Text } from "react-native";
-import {Link} from "expo-router";
-import {SafeAreaView as RNSaveAreaView} from "react-native-safe-area-context";
-import {styled} from "nativewind";
+import { SafeAreaView as RNSaveAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSaveAreaView);
 
 export default function App() {
-    return (
-        <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
-            </Text>
-            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white">
-                Go to Onboarding
-            </Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white">
-                Go to Sign in
-            </Link>
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white">
-                Go to sign up
-            </Link>
-            <Link href="/subscriptions/1" className="mt-4 rounded bg-primary text-white">
-                Go to sign up
-            </Link>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-5xl font-sans-extrabold text-primary ">Home</Text>
+      <Link
+        href="/onboarding"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Onboarding
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Sign in
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to sign up
+      </Link>
+    </SafeAreaView>
+  );
 }
